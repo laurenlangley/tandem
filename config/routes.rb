3c4c68d6952632c2c01cf015f08b1eacdeb7f959
Jason B <devilshaircut@gmail.com>
2014-04-12 18:31:51 -0400

    Resolve merge conflict.

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,11 @@ Tandem::Application.routes.draw do
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   
-<<<<<<< HEAD
-  get   '*path'                    => "home#index"
-
-=======
   devise_scope :user do
     get 'sign_in', :to => 'devise/sessions#new', :as => :new_user_session
     get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
   end
   
   get  '*path'                    => "home#index"
->>>>>>> cfb881b93a6cd72eea3fc10c4b354924ee2c3403
+
 end
