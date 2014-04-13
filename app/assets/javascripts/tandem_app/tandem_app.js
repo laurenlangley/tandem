22492410c8e52c98da02c59714202a696a825c96
Mitchell Lane <mitchell@fullscreen.net>
2014-04-13 03:11:47 -0400

    Local Storage-ing filters

--- app/assets/javascripts/tandem_app/tandem_app.js
+++ app/assets/javascripts/tandem_app/tandem_app.js
@@ -5,40 +5,45 @@ var TandemApp = angular.module("TandemApp", ["ngRoute", "ui.bootstrap", "ngSanit
 // Global Modal Open.
 TandemApp.run(["$rootScope", function($rootScope) {
 
-  $rootScope.filters = {
-    "minAge":             18,
-    "maxAge":             100,
-    "gender":             ["Male", "Female", "Other", "Any"],
-    "location": {
-       "offroad":         true,
-       "paved_trail":     true,
-       "road":            true
-    },
-    "scene": {
-       "weekend_warrior": true,
-       "commuter":        true,
-       "mountain":        true,
-       "fixies":          true,
-       "social":          true,
-       "roadie":          true
-    },
-    "skill":              "beginner",
-    "looking_for": {
-       "training":        true,
-       "fun":             true,
-       "commuting":       true,
-       "destination":     true
-    },
-    "availability": {
-       "sunday":          "All",
-       "monday":          "All",
-       "tuesday":         "All",
-       "wednesday":       "All",
-       "thursday":        "All",
-       "friday":          "All",
-       "saturday":        "All"
-    }
-  };
+  console.log( localStorage["filters"] )
+  if( localStorage["filters"] )
+    $rootScope.filters = JSON.parse( localStorage["filters"] );
+  else{
+    $rootScope.filters = {
+      "minAge":             18,
+      "maxAge":             100,
+      "gender":             ["Male", "Female", "Other", "Any"],
+      "location": {
+         "offroad":         true,
+         "paved_trail":     true,
+         "road":            true
+      },
+      "scene": {
+         "weekend_warrior": true,
+         "commuter":        true,
+         "mountain":        true,
+         "fixies":          true,
+         "social":          true,
+         "roadie":          true
+      },
+      "skill":              "beginner",
+      "looking_for": {
+         "training":        true,
+         "fun":             true,
+         "commuting":       true,
+         "destination":     true
+      },
+      "availability": {
+         "sunday":          "All",
+         "monday":          "All",
+         "tuesday":         "All",
+         "wednesday":       "All",
+         "thursday":        "All",
+         "friday":          "All",
+         "saturday":        "All"
+      }
+    };
+  }
 
   $rootScope.cycleAvailability = function(day, time) {
     if ( time == "Morning" ) {
