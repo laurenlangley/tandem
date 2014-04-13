354f4de278c9f5b04593abbe6afb36a0d36fcf3d
Mitchell Lane <mitchell@fullscreen.net>
2014-04-12 21:11:42 -0400

    A Few more things to make it work on heroku

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -3,3 +3,9 @@
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
 # Mime::Type.register_alias "text/html", :iphone
+
+
+Mime::Type.register 'font/opentype', :otf
+Mime::Type.register 'font/truetype', :ttf
+Mime::Type.register 'application/vnd.ms-fontobject', :eot
+Mime::Type.register 'application/x-font-woff', :woff
