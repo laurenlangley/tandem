361904c5cca9673c6db334c4da31a582604eb2bf
Mitchell Lane <mitchell@fullscreen.net>
2014-04-12 22:09:53 -0400

    Cleaning up seeds

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -231,7 +231,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :facebook, "1434652683448583", "ff4d68a6dcd346dd8ca446523a8c7338", :scope => 'email,user_birthday'
+  config.omniauth :facebook, "1434652683448583", "ff4d68a6dcd346dd8ca446523a8c7338", :scope => 'email,user_birthday,user_location'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 
 User.delete_all
 
-valid_availabilities = ["", "12am-4am", "4am-8am", "8am-12pm", "12pm-4pm", "4pm-8pm", "8pm-12am"]
+valid_availabilities = ["", "Morning", "Afternoon", "Evening"]
 
 25.times do |i|
   User.create({
@@ -19,29 +19,22 @@ valid_availabilities = ["", "12am-4am", "4am-8am", "8am-12pm", "12pm-4pm", "4pm-
       :age    => ( rand(25) + 18 ),
       :gender => ( rand(2) == 0 ? "male" : "female" ),
       :location => {
-        :road_warrior     => rand(2) == 0,
-        :trails_only      => rand(2) == 0,
-        :silver_comet     => rand(2) == 0,
-        :city             => rand(2) == 0,
-        :stone_mountain   => rand(2) == 0,
-        :beltline         => rand(2) == 0
+        :offroad          => rand(2) == 0,
+        :paved_trail      => rand(2) == 0,
+        :road             => rand(2) == 0
       },
       :scene => {
         :weekend_warrior  => rand(2) == 0,
         :commuter         => rand(2) == 0,
         :mountain         => rand(2) == 0,
-        :fixed            => rand(2) == 0
+        :fixies           => rand(2) == 0,
+        :social           => rand(2) == 0
+        :roadie           => rand(2) == 0
       },
       :skill  => {
         :beginner         => false,
         :intermediate     => true,
         :advanced         => false
-      },
-      :speed  => {
-        :casual           => rand(2) == 0,
-        :commuter         => rand(2) == 0,
-        :exercise         => rand(2) == 0,
-        :race             => rand(2) == 0
       },
       :looking_for => {
         :training         => rand(2) == 0,
@@ -50,14 +43,15 @@ valid_availabilities = ["", "12am-4am", "4am-8am", "8am-12pm", "12pm-4pm", "4pm-
         :destination      => rand(2) == 0
       },
       :availability => {
-        :sunday     => valid_availabilities[ rand(7) ],
-        :monday     => valid_availabilities[ rand(7) ],
-        :tuesday    => valid_availabilities[ rand(7) ],
-        :wednesday  => valid_availabilities[ rand(7) ],
-        :thursday   => valid_availabilities[ rand(7) ],
-        :friday     => valid_availabilities[ rand(7) ],
-        :saturday   => valid_availabilities[ rand(7) ]
-      }
+        :sunday     => valid_availabilities[ rand(4) ],
+        :monday     => valid_availabilities[ rand(4) ],
+        :tuesday    => valid_availabilities[ rand(4) ],
+        :wednesday  => valid_availabilities[ rand(4) ],
+        :thursday   => valid_availabilities[ rand(4) ],
+        :friday     => valid_availabilities[ rand(4) ],
+        :saturday   => valid_availabilities[ rand(4) ]
+      },
+      :summary => Faker::Lorem.paragraph
     }.to_json
   })
 end
