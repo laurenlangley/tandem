7f309b3460011d28d49828d0cc355b3e8a16204f
Jason B <devilshaircut@gmail.com>
2014-04-13 04:13:54 -0400

    More icons and get the background on time filters to work.

--- app/assets/javascripts/tandem_app/controllers/filterController.js
+++ app/assets/javascripts/tandem_app/controllers/filterController.js
@@ -4,7 +4,23 @@ TandemApp.controller("filterController", ["$scope", "httpService", "$rootScope",
   $scope.dataModel = {};
 
 
-
+  $scope.evaluateDay = function(time) {
+    if ( time == "Morning" ) {
+      return "tm-match-day-morning"
+    }
+    else if ( time == "Afternoon" ) {
+      return "tm-match-day-afternoon";
+    }
+    else if ( time == "Evening" ) {
+      return "tm-match-day-evening";
+    }
+    else if ( time == "All" ) {
+      return "tm-match-day-all";
+    }
+    else {
+      return "tm-match-day-none";
+    }
+  };
 
 
   // Initiate the HTTP request.
